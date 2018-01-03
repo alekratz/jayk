c79440e99a39a02ca410fae1017bc17cc18ff862
Max Marrone <max@marrone.nyc>
2018-01-03 10:19:12 -0500

    Fix dumb misspellings in whatbot.py.

--- examples/whatbot.py
+++ examples/whatbot.py
@@ -7,13 +7,13 @@ def message_is_what(message):
 
 class Whatbot(metaclass=JaykMeta):
     def __init__(self, **kwargs):
-        super().init(**kwargs)
+        super().__init__(**kwargs)
         self._last_message = None
         self._last_sender = None
     
     def on_message(self, client, room, sender, msg):
         if message_is_what(msg) and self._message_to_repeat is not None:
-            reply = sender + ": " + self._message_to_repeat.upper()
+            reply = sender.nick + ": " + self._message_to_repeat.upper()
             client.send_message(room, reply)
         else:
             self._message_to_repeat = msg
