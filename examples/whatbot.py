0d5e8fe7dfb8c9ad66ac5dcbb253ef62d41748e7
Max Marrone <max@marrone.nyc>
2018-01-03 09:58:49 -0500

    Add whatbot.py.

---
+++ examples/whatbot.py
@@ -0,0 +1,19 @@
+from jayk.cli.module import JaykMeta
+import string
+
+def message_is_what(message):
+    filtered_message = "".join(c for c in message if c not in (string.punctuation+string.whitespace))
+    return filtered_message.lower() == "what"
+
+class Whatbot(metaclass=JaykMeta):
+    def __init__(self, **kwargs):
+        super().init(**kwargs)
+        self._last_message = None
+        self._last_sender = None
+    
+    def on_message(self, client, room, sender, msg):
+        if message_is_what(msg) and self._message_to_repeat is not None:
+            reply = sender + ": " + self._message_to_repeat.upper()
+            client.send_message(room, reply)
+        else:
+            self._message_to_repeat = msg
